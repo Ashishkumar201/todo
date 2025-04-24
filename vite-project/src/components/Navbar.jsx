ab6edfad05562c8a2a30809d07bda6cd9a68568f
Ashishkumar201 <ashishkumarbagria@gmail.com>
2025-04-24 19:02:18 +0530

    nav

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <nav className='flex justify-between bg-blue-900 text-white py-3 pt-3 my-2 mx-8 rounded-lg ml-8 pl-5 '>
         <div className="logo">
-            <span className="font-bold mx-8 text-xl">iTask</span>
+            <span className="font-bold mx-8 text-xl">iTask: your Task Manager</span>
         </div>
         <ul className="flex gap-8 mx-9">
             <li className='cursor-pointer  hover:text-blue-500 transition-all'>Home</li>
